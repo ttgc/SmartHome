f6ea0334d81b222b89129a5f33ddff05cdea08a3
ttgc <ttgc@users.noreply.github.com>
2020-07-07 15:06:55 -0400

    Added conversion class

---
+++ Edge/Utils/Conversion/FahrenheitToCelsius.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edge.Utils.Conversion
+{
+    public class FahrenheitToCelsius : IDoubleConverter
+    {
+        public double Convert(double value)
+        {
+            return (value - 32) * (5.0 / 9.0);
+        }
+    }
+}
---
+++ Edge/Utils/Conversion/IConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edge.Utils.Conversion
+{
+    /// <summary>
+    /// Interface for multiple conversion
+    /// </summary>
+    /// <typeparam name="FROM">The origin type of the data</typeparam>
+    /// <typeparam name="TO">The output type of the data</typeparam>
+    internal interface IConverter<FROM, TO>
+    {
+        /// <summary>
+        /// Convert a value to another
+        /// </summary>
+        /// <param name="value">The value to be converted</param>
+        /// <returns>The value converted</returns>
+        TO Convert(FROM value);
+    }
+
+    internal interface IDoubleConverter : IConverter<double, double> { }
+}
