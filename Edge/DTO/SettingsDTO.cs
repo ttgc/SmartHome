cf8847d93b8938d3243bccde6fb8711027e0aa1a
ttgc <ttgc@users.noreply.github.com>
2020-07-07 16:56:42 -0400

    Added settings operation get/post

---
+++ Edge/DTO/SettingsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edge.DTO
+{
+    public class SettingsDTO
+    {
+        public string house_name { get; set; }
+        public double? clim_on { get; set; }
+        public double? clim_off { get; set; }
+        public double? heat_on { get; set; }
+        public double? heat_off { get; set; }
+        public char? temperature_unit { get; set; }
+    }
+}
