2098acec5472905086d26cb00569429a2ac849e9
ttgc <ttgc@users.noreply.github.com>
2020-07-07 15:57:47 -0400

    Finished post operation

--- Edge/Controllers/TemperatureController.cs
+++ Edge/Controllers/TemperatureController.cs
@@ -29,24 +29,41 @@ namespace Edge.Controllers
         [HttpPost("{value}")]
         public IActionResult Post(double value)
         {
-            context.Temperature.Add(new Temperature() { Timer = DateTime.Now, Val = value });
-            context.SaveChanges();
+            Temperature previous = context.Temperature.OrderBy(s => s.Timer).Last();
+            if (previous == null)
+            {
+                previous = new Temperature() { 
+                    Timer = DateTime.MinValue, ClimOn = false, HeatOn = false 
+                }; 
+            }
 
             Settings settings = context.Settings.First();
             PostTemperatureOutputDTO dto = new PostTemperatureOutputDTO() { 
-                clim_on = value >= settings.ClimOn /*&& value >= settings.ClimOff*/,
-                heat_on = value <= settings.HeatOn /*&& value <= settings.HeatOff*/
+                clim_on = value >= settings.ClimOn || (previous.ClimOn.Value && value >= settings.ClimOff),
+                heat_on = value <= settings.HeatOn || (previous.HeatOn.Value && value <= settings.HeatOff)
             };
+
+            context.Temperature.Add(new Temperature() { 
+                Timer = DateTime.Now, Val = value, ClimOn = dto.clim_on, HeatOn = dto.heat_on
+            });
+            context.SaveChanges();
+
 
             Stattemperature stats = context.Stattemperature.Find(DateTime.Now.ToString("yyyy"));
             if (stats == null)
             {
-                stats = new Stattemperature() { Statyear = DateTime.Now.ToString("yyyy") };
+                stats = new Stattemperature() { 
+                    Statyear = DateTime.Now.ToString("yyyy"), 
+                    ClimDuration = new TimeSpan(), 
+                    HeatDuration = new TimeSpan()
+                };
                 context.Add(stats);
             }
             IQueryable<Temperature> query = context.Temperature.Where(s => s.Timer.Year == DateTime.Now.Year);
             stats.Average = query.Average(s => s.Val);
             stats.StdDeviation = query.Sum(s => s.Val - stats.Average) / query.Count();
+            if (previous.HeatOn.Value) stats.HeatDuration += (DateTime.Now - previous.Timer);
+            if (previous.ClimOn.Value) stats.ClimDuration += (DateTime.Now - previous.Timer);
             context.SaveChanges();
 
             return Ok(dto);
